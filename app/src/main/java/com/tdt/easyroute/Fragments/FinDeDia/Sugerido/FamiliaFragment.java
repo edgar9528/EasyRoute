110a0afdbbe9cb2996a71fef7ccbb76cf94dd81c
edgar <edgar9528@gmai.com>
2020-01-15 15:56:04 -0600

    Actualiza interfaz pedidos2 3:50PM 15ENE

--- app/src/main/java/com/tdt/easyroute/Fragments/FinDeDia/Sugerido/FamiliaFragment.java
+++ app/src/main/java/com/tdt/easyroute/Fragments/FinDeDia/Sugerido/FamiliaFragment.java
@@ -61,7 +61,7 @@ public class FamiliaFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        //inflater.inflate(R.menu.main_menu2, menu);
+        //inflater.inflate(R.menu.sugerido_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
--- app/src/main/java/com/tdt/easyroute/Fragments/FinDeDia/Sugerido/MainsugeridoFragment.java
+++ app/src/main/java/com/tdt/easyroute/Fragments/FinDeDia/Sugerido/MainsugeridoFragment.java
@@ -45,7 +45,7 @@ public class MainsugeridoFragment extends Fragment {
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.main_menu2, menu);
+        inflater.inflate(R.menu.sugerido_menu, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 
